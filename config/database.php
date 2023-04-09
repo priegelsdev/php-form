3c74df4b04fd3afde4d3c52d0e85b34be848d15f
priegelsdev <priegelsdev@gmail.com>
2023-04-09 10:54:12 +0200

    connect to mysql db

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mrtest');
+define('DB_PASS', '1234');
+define('DB_NAME', 'php_dev');
+
+// create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// check conn
+if ($conn->connect_error) {
+  die('connection failed ' . $conn->connect_error);
+}
+
+echo 'CONNECTED';

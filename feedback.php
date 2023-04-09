7dbc996eb210c4c03f8fcc7de5f308b3290b641b
priegelsdev <priegelsdev@gmail.com>
2023-04-09 11:17:36 +0200

    fetch data from mysql db and display on feedback page with formatted date

--- feedback.php
+++ feedback.php
@@ -1,27 +1,32 @@
 <?php include 'inc/header.php'; ?>
 
+<?php
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
 
-<div class="container d-flex flex-column align-items-center">
 
-  <h2>Feedback</h2>
+<div class="container">
 
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+  <h2>Past Feedback</h2>
+
+  <?php if (empty($feedback)) : ?>
+    <p>There is no fb</p>
+  <?php endif; ?>
+
+  <?php foreach ($feedback as $item) : ?>
+
+    <div class="card">
+      <div class="card-body">
+        <?php echo $item['body']; ?>
+        <div class="card-author">
+          By <?php echo $item['name']; ?> on <?php $newDate = date("d.m.Y", strtotime($item['date']));
+                                              echo $newDate; ?>
+        </div>
+      </div>
     </div>
-  </div>
-
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-    </div>
-  </div>
-
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-    </div>
-  </div>
+  <?php endforeach; ?>
 </div>
 </main>
 </body>
